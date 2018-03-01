cb588f0db65d35cfcb143f9a417b592fc797cfa5
Wolfe <mlwolfe@my.loyno.edu>
2018-03-01 10:41:17 -0600

    Almost finished
    Design elements are complete. Must add java

---
+++ script.js
@@ -0,0 +1,36 @@
+
+
+$(document).ready(function(){
+	
+	$("#overlay-index").click(function(){
+		$("#overlay").fadeIn(100);	
+		
+	});
+	
+		$("#overlay-tour").click(function(){
+		$("#overlay").fadeIn(100);	
+		
+	});
+	
+		$("#overlay-music").click(function(){
+		$("#overlay").fadeIn(100);	
+		
+	});
+	
+		$("#overlay-store").click(function(){
+		$("#overlay").fadeIn(100);	
+		
+	});
+	
+	
+	$("#overlay-close").click(function(){
+		$("#overlay").fadeOut(100);
+		
+	});
+	
+
+});
+
+
+
+	
